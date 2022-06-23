fb710a9a2ece2fe907430dcd28d7ed7e60c4c86c
Shivangi Sangwan <66208300+shivi0@users.noreply.github.com>
2022-06-24 00:28:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const userRoutes = require('./routes/users');
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/camp-zone';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
@@ -114,7 +114,7 @@ app.use(
                 "'self'",
                 "blob:",
                 "data:",
-                "https://res.cloudinary.com/daz3jddx9/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://res.cloudinary.com/YOUR_USER_NAME/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
                 "https://images.unsplash.com/",
             ],
             fontSrc: ["'self'", ...fontSrcUrls],
